926239683cff877eb9c75ac0300abb3126b9c0e9
shivani21goyal <shivani1996goyal@gmail.com>
2017-02-15 22:55:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,13 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="new value";
 //move image
-var img=document.getElementById(img);
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function()
 {
   img.style.marginLeft='100px';  
