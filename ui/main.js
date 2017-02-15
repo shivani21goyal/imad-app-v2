f28c82565ce3b6345a34427a5beadfc0e3e66f4d
shivani21goyal <shivani1996goyal@gmail.com>
2017-02-15 22:38:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
-element.insertHTML="new value";
+element.innerHTML="new value";
 //move image
 var img=document.getElementById(img);
 img.onclick=function()
