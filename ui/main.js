c45692d8ebb8a75879acf2d65c7d99121f26ccb8
shivani21goyal <shivani1996goyal@gmail.com>
2017-02-16 21:34:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,18 +7,19 @@ button.onclick=function()
     //create a request object
     request.onreadystatechange=function()
     {
-    if(request.readystate==XMLHttpRequest.DONE)
+    if(request.readyState==XMLHttpRequest.DONE)
     {
     //take some action
     if(request.status==200)
     {
-        var counter=request.responseTest;
+        var counter=request.responseText;
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();
     }
     }
     //not done yet
-};request.open('GET','http://shivani21goyal.imad.hasura-app.io/counter',true);
+};
+request.open('GET','http://shivani21goyal.imad.hasura-app.io/counter',true);
 request.send(null);
 };
 
